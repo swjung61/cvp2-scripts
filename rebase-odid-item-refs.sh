16b5c6a911b36606d7170d31d075a7fc230d1963
Craig Pratt <craig@ecaspia.com>
2013-10-21 15:30:40 -0700

    Added sanity checks to ensure the script is in the right kind of directory

--- rebase-odid-item-refs.sh
+++ rebase-odid-item-refs.sh
@@ -2,7 +2,7 @@
 # Note: This script is assumed to be in the odid directory containing 
 #       the "item-refs" and "media" subdirectories. For convenience, 
 #       include this script with redistributable odid trees (at least
-#       until we can get rid of abolute paths and/or .item files...
+#       until we can get rid of abolute paths and/or .item files...)
 
 scriptdir=$(dirname $0)
 # note: $0 (and scriptdir) may be relative - need to make it absolute...
@@ -10,6 +10,16 @@ cd "$scriptdir"
 
 # $basedir will be absolute
 basedir="$PWD"
+
+if [ ! -d item-refs ]; then
+   echo "Couldn't find directory 'item-refs'. Make sure this script is an odid base directory."
+   exit 1
+fi
+
+if [ ! -d media ]; then
+   echo "Couldn't find directory 'media'. Make sure this script is an odid base directory."
+   exit 2
+fi
 
 echo "Rebasing odid item references to $basedir/odid/media..."
 
