0d0a6b30517819fb2c1ba678ea7a425393871b5e
Parthiban Balasubramanian <p.balasubramanian@cablelabs.com>
2013-12-03 14:51:09 -0700

    Removing dtcp library dependency from the script

--- bootstrap.sh
+++ bootstrap.sh
@@ -228,27 +228,6 @@ process_package()
     make install 2>&1 | tee -a ${log_file} || bailout "Couldn't install ${package_base}"
 }
 
-install_dtcp_lib()
-{
-    echo "*** Processing dtcp-rygel Repository: ${1}" 2>&1 | tee -a ${log_file}
-
-    local repo_url="$1"
-    local repo_branch="$2"
-    local repo_base=$(basename ${repo_url} .git)
-
-    echo "*** Installing ${repo_base}..." 2>&1 | tee -a ${log_file}
-    cd ${CVP2_GIT} || bailout "Couldn't cd to ${CVP2_GIT}"
-    git clone ${repo_url} | tee -a ${log_file} || bailout "Couldn't clone ${repo_base}"
-    cd ${repo_base} || bailout "Couldn't cd to ${repo_base} directory"
-    git checkout ${repo_branch}
-
-    echo "*** Installing dtcp library files" | tee -a ${log_file}
-    cp include/dtcpip.h $dtcpip_header | tee -a ${log_file}
-    cp lib/libdtcpip.so $dtcpip_lib | tee -a ${log_file}
-    cp vapi/dtcpip.vapi ${CVP2_ROOT}/share/vala/vapi | tee -a ${log_file}
-    echo "*** Done copying dtcp library files."
-}
-
 process_repo()
 {
 	echo "*** Processing Repository: ${1}" 2>&1 | tee -a ${log_file}
@@ -278,37 +257,6 @@ for ((i=0; i<=$(($num_ext_pkgs-1)); i++))
 do
 	process_package "${external_packages[i]}" "${external_packages[++i]}"
 done
-
-# Setup dtcpip env variables
-dtcp_rygel_repo="git@bitbucket.org:cvp2ri/dtcp-rygel.git"
-dtcpip_lib="${CVP2_ROOT}/lib/libdtcpip.so"
-dtcpip_header="${CVP2_ROOT}/include/dtcpip.h"
-dtcpip_vapi="${CVP2_ROOT}/vapi/dtcpip.vapi"
-install_dtcp_lib "$dtcp_rygel_repo" "$default_branch"
-
-dtcpip_pc="${CVP2_ROOT}/lib/pkgconfig/dtcpip.pc"
-
-echo "*** Creating dtcpip.pc file." | tee -a ${log_file}
-echo $dtcpip_pc
-# creating dtcpip.pc file
-cat > $dtcpip_pc << EndOfFile
-prefix=${CVP2_ROOT}
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-datarootdir=\${prefix}/share
-datadir=\${datarootdir}
-
-Name: dtcpip
-Description: DTCP
-Version: 1.0
-Requires:
-Libs: -L\${libdir} -ldtcpip
-Cflags: -I\${includedir}
-EndOfFile
-
-# Log environment
-cat ${dtcpip_pc} | tee -a ${log_file}
 
 # CVP2 controlled repositories
 num_cvp2_repos=${#cvp2_repos[*]}
